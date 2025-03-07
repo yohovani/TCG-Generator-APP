87c13788e10f3f987bc748997ebc82db99785a8c
Yohovani Vargas <yohovanivargas@gmail.com>
2025-03-06 19:00:42 -0600

    add drawables and card activity

---
+++ app/src/main/java/yoho/tcggenerator/card_select.kt
@@ -0,0 +1,24 @@
+package yoho.tcggenerator
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class card_select : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_card_select)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
+
+open class AppCompatActivity {
+
+}
